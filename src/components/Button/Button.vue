b252d3a9ebd4f5343aa80a029067cc76c1f6503a
Ye Jia <ye.jia.alice@gmail.com>
2017-02-27 22:54:12 +0800

    6. Vue.use的用法

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,16 @@
+<template>
+	<button><slot></slot></button>
+</template>
+<style>
+	button {
+		background: blue;
+		color: #fff;
+		border: 0;
+		width: 100px;
+		height: 30px;
+		border-radius: 10px;
+	}
+</style>
+<script>
+	
+</script>
